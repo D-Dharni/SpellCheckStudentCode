c4c99e0801c9d8c759addaee311ddcfa44ca067b
devendharni <deven.dharni@gmail.com>
2025-09-27 17:38:36 -0700

    -Did a lot of thinking and updated the process in the work log -Put some rough pseudocode logical outline in the code

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -19,6 +19,35 @@ public class SpellCheck {
      */
     public String[] checkWords(String[] text, String[] dictionary) {
 
+        // PART 1: Setting up Framework
+
+        // Create array of arrayLists - shelves for the library
+
+        // Loop through the array and initialize each arrayList
+
+        // Loop through each word in dictionary
+
+            // Calculate value using formula: value = (value*29) + ASCII value
+
+            // Modulo by the number of shelves to put number back in range
+
+            // Add the word to the correct shelf in the framework
+
+        // PART 2: Spell Checking
+
+        // Loop through each word in the text
+
+            // Calculate the value using formula
+
+            // Modulo back in range
+
+            // Search the shelf using some algorithm
+
+                // If it's not in there add it to the list of non-correct
+
+
+
+        // Return the final list
         return null;
     }
 }
